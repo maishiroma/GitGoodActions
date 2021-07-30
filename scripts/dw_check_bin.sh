84c7dabf1dd6dd3b279502854a929d7b86dc4f5d
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-07-30 13:03:00 -0700

    Test on new shasum

--- scripts/dw_check_bin.sh
+++ scripts/dw_check_bin.sh
@@ -29,11 +29,13 @@ wget -q -O ${bin_dw_loc} ${bin_url}
 wget -q -O ${shasum_dw_loc} ${sha256_url}
 
 # Shasum checker
-check=$(cat ${shasum_dw_loc} | grep "$(sha256sum ${bin_dw_loc} | cut -d ' ' -f 1)")
-if [ ! -n "${check}" ]; then
+cd /tmp/
+check=$(shasum -c --ignore-missing ${shasum_dw_loc} | grep -c "FAILED")
+if [ ${check} -eq 1 ]; then
     echo "::error ::sha256 of ${bin_actual_name} does not match with official download, please try again."
     exit 1
 fi
+cd -
 
 if [[ "${bin_dw_name##*.}" == "zip" ]]; then
     # If the file is a zip, we extract it and presume that the contents contains
